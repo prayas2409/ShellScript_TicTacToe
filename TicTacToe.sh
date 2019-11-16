a3186eadc471683acf8c702eb5147e4655e69d7c
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-11-16 11:05:09 +0530

    Added functionality to initialize and show the board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,3 +2,28 @@
 
 declare -A board
 board_Size=3
+
+function initialize_Board()
+for (( row=1; row<4 ; row++ )) do
+      for (( column=1; column<4 ; column++ )) do
+#         read var
+			board[$row,$column]=0
+      done
+done
+
+function show_Board(){
+	for (( row=1; row<4 ; row++ )) do
+   	for (( column=1; column<4 ; column++ )) do
+			if [ ${board[$row,$column]}==0 ]
+         then
+            printf  _" "
+        	else
+            printf ${board[$row,$column]}" "
+         fi
+		done
+   echo
+done
+}
+
+initialize_Board
+show_Board
