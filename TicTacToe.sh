f6cb5852fd18d16ecb8854a36bf980bed80ce124
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-11-16 12:04:39 +0530

    Toss and assign a sign to user

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,6 +2,8 @@
 
 declare -A board
 board_Size=3
+USER_SIGN=0
+COMP_SIGN=0
 
 function initialize_Board()
 for (( row=1; row<4 ; row++ )) do
@@ -25,5 +27,16 @@ function show_Board(){
 done
 }
 
+function toss_Assign_Sign(){
+	if [ $((RANDOM%2))==1 ]
+	then
+			USER_SIGN=X
+	else
+			COMP_SIGN=X
+	fi
+	echo "Your sign is "$USER_SIGN" and computer sign is "$COMP_SIGN
+}
+
 initialize_Board
+toss_Assign_Sign
 show_Board
